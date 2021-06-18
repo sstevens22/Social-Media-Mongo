cdf0963bf0785ede445c2dcd0cb23b136229ef86
Sheldon Stevens <sgste080@yahoo.com>
2021-06-18 11:14:05 -0700

    A thing

---
+++ routes/api/user-routes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/user-controller");
+
+router.route("/").get(getAllUsers).post(createUser);
+
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+// Add and delete a friend
+router.route("/:id/friends/:friendsId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
